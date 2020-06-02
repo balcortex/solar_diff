9f840d66430a1b2df0ed7955a6d8b57e18bd887b
Bal Cortex <balcortex@hotmail.com>
2020-06-01 20:04:26 -0500

    update data_manager

--- src/data_manager.py
+++ src/data_manager.py
@@ -1,7 +1,10 @@
-import pandas as pd
-from typing import NamedTuple
-
 from __future__ import annotations
+
+import random
+from typing import List, NamedTuple
+
+import pandas as pd
+import zipfile
 
 
 class Dataset(NamedTuple):
@@ -9,24 +12,35 @@ class Dataset(NamedTuple):
     test: pd.DataFrame
 
 
-class DataManager:
+# class DataModel(NamedTuple):
+#     real: Dataset
+#     diff: Dataset
+#     csky: Dataset
+
+
+class Indices(NamedTuple):
+    train_drop: List(int)
+    test: List(int)
+
+
+class DataLoader:
     def __init__(self, csv_path: str) -> None:
         self._csv_path = csv_path
 
         self._train = None
         self._test = None
+        self._train_indices_drop = None
+        self._test_indices = None
 
-    def load(self) -> DataManager:
+    def read_csv(self) -> DataLoader:
         "Load and process the csv file containing the database"
-        self._train = pd.load_csv(self._csv_path)
+        self._train = pd.read_csv(self._csv_path)
         # We don't need the first column (index)
         self._train.drop(columns=["Unnamed: 0"], inplace=True)
 
         return self
 
-    def split_train_test(
-        self, num_levels: int = 10, seed: int = 42, sort_by_level: bool = True
-    ) -> Dataset:
+    def split_train_test(self, train_drop: List[int], test: List[int]) -> DataLoader:
         """Split database into training and test sets.
         The resulting test set contains representative days with different amounts of cumulative irradiance measurements (e.g., extreme cloudy days, cloudy days, sunny days).
         All measurements included in the test set are dropped from the training set.
@@ -37,8 +51,99 @@ class DataManager:
             sort_by_level: sort the test set from lower cumulative irradiance (False=random order)
 
         """
-        if self._train and not self._test:
-            # self._train =
-            # self._test =
-            pass
+
+        self._test = self._train.iloc[test]
+        self._test.reset_index(drop=True, inplace=True)
+
+        self._train.drop(train_drop, inplace=True)
+        self._train.reset_index(drop=True, inplace=True)
+
+        return self
+
+    def get_indices(
+        self, num_levels: int = 10, seed: int = 42, sort_by_level: bool = True
+    ) -> Indices:
+
+        if seed:
+            random.seed(seed)
+
+        irr_sum = []
+        indices = []
+
+        # Each row contains 144 measurements (3 whole days in total)
+        for index in range(0, len(self._train), 144):
+            # We only want to compare the last day of the row (i.e. the outputs)
+            output_day = self._train.iloc[index].values[-48:]
+            indices.append(index)
+            irr_sum.append(sum(output_day))
+
+        irr_min = min(irr_sum)
+        irr_max = max(irr_sum)
+        bin_size = (irr_max - irr_min) / (num_levels - 1)
+
+        # Convert sum to categories
+        irr_categorical = [int(i // bin_size) for i in irr_sum]
+
+        # Add randomly one day of each category
+        # pylint: disable=unsubscriptable-object
+        combined = list(zip(irr_categorical, indices))
+        count = 0
+        test_levels = []
+        test_indices = []
+        while count < num_levels:
+            samp = random.sample(combined, 1)[0]
+            if samp[0] not in test_levels:
+                test_levels.append(samp[0])
+                test_indices.append(samp[1])
+                count += 1
+
+        # Sort from lower to higher level of irradiance
+        if sort_by_level:
+            test_levels, test_indices = zip(*sorted(zip(test_levels, test_indices)))
+            test_levels = list(test_levels)
+            test_indices = list(test_indices)
+
+        # Drop every portion of the test set from the training set
+        indices_to_drop = []
+        for ind in test_indices:
+            temp_index = list(range(ind, ind + 144))
+            indices_to_drop.extend(temp_index)
+
+        self._train_indices_drop = indices_to_drop
+        self._test_indices = test_indices
+
+        return Indices(indices_to_drop, test_indices)
+
+    def get_dataset(self):
         return Dataset(self._train, self._test)
+
+    def save_csv(self, path: str) -> None:
+        self._train.to_csv(path + "_train.csv")
+        self._test.to_csv(path + "_test.csv")
+
+
+if __name__ == "__main__":
+    AMARILLO_PATH = "/home/bal/Dropbox/Solar_Forecasting/texas_amarillo/amarillo_db.csv"
+    AMARILLO_DIFF_PATH = (
+        "/home/bal/Dropbox/Solar_Forecasting/texas_amarillo/amarillo_db_diff.csv"
+    )
+
+    amarillo = DataLoader(AMARILLO_PATH)
+    amarillo_diff = DataLoader(AMARILLO_PATH)
+    amarillo.read_csv()
+    amarillo_diff.read_csv()
+
+    indices = amarillo.get_indices()
+
+    amarillo.split_train_test(indices.train_drop, indices.test)
+    amarillo_diff.split_train_test(indices.train_drop, indices.test)
+
+    assert list(amarillo.get_dataset().test.loc[9, "y36":"y38"].values) == [
+        349,
+        243,
+        142,
+    ]
+
+    # amarillo.save_csv("amarillo")
+    # amarillo_diff.save_csv("amarillo_diff")
+    print("Done")
