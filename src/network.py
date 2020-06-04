99ae06b0519bc3b2dd0a0a2d12217ee212946367
Bal Cortex <balcortex@hotmail.com>
2020-06-03 21:10:36 -0500

    update network

--- src/network.py
+++ src/network.py
@@ -1,12 +1,22 @@
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Dense
-from typing import Sequence
+from tensorflow.keras.layers import Dense, Input
+from typing import Sequence, Optional
 
 
 class MLP(Model):
-    def __init__(self, hidden: Sequence[int]):
+    def __init__(
+        self,
+        input_size: int,
+        output_size: int,
+        hidden: Sequence[int],
+        hidden_activation: Optional[str] = "relu",
+    ):
         super(MLP, self).__init__()
-        self.blocks = [Dense(i) for i in hidden]
+        self._input_size = input_size
+        self._output_size = output_size
+        self.blocks = [Input(shape=(input_size,))]
+        self.blocks.extend([Dense(i, activation=hidden_activation) for i in hidden])
+        self.blocks.append(Dense(output_size))
 
     def call(self, inputs):
         for block in self.blocks:
